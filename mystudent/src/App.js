477539cc37b7fd7d60e68d1f96a8545c2dec8a90
jantapabh <jantapa2407@gmail.com>
2020-03-11 20:46:34 +0700

    add app

--- mystudent/src/App.js
+++ mystudent/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import StudentCard from './components/StudentCard'
+import StudentList from './components/StudentList'
+import InputForm from './components/InputForm'
+import { useSelector, useDispatch } from 'react-redux'
 
-function App() {
+export default () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Students</h2>
+      <StudentList />
+      <InputForm  />
     </div>
-  );
+  )
 }
-
-export default App;
