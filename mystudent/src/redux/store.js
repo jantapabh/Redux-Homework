ebcbc4e9eccbbdc17f1bf70cf4beff990688f5e2
jantapabh <jantapa2407@gmail.com>
2020-03-11 20:35:59 +0700

    add store finish

--- mystudent/src/redux/store.js
+++ mystudent/src/redux/store.js
@@ -9,46 +9,54 @@ const initialform = {
     surname: '',
     faculty: '',
     advisor: ''
-    
+
 }
 
 const formReducer = (data = initialform, action) => {
-    switch(action.type){
-        case 'CHANGE_NAME' : 
-            return {...data,name: action.name}
-        case 'CHANGE_WEIGHT' : 
-            return {...data,weight: action.weight}
-        case 'CHANGE_IMG' : 
-            return {...data,img: action.img}
+
+    switch (action.type) {
+
+        case 'CHANGE_GENERATION':
+            return { ...data, generation: action.generation }
+        case 'CHANGE_ID':
+            return { ...data, idStudent: action.idStudent }
+        case 'CHANGE_NAME':
+            return { ...data, name: action.name }
+        case 'CHANGE_SURNAME':
+            return { ...data, surname: action.surname }
+        case 'CHANGE_FACULTY':
+            return { ...data, faculty: action.faculty }
+        case 'CHANGE_ADVISOR':
+            return { ...data, advisor: action.advisor }
         default: return data;
     }
-    
+
 }
 
 
-const bearReducer = (bears = [], action) => {
+const studentReducer = (students = [], action) => {
     switch (action.type) {
-        case 'GET_BEARS':
-            return action.bears;
-        case 'ADD_BEAR':
-            return [...bears,action.bear]
-        case 'DELETE_BEAR':
-            return bears.filter(bear=> +bear.id !== +action.id)
-        case 'UPDATE_BEAR':
-            return bears.map(bear => {
-                if(+bear.id === +action.id)
-                    return action.bear;
-                else 
-                    return bear;
+        case 'GET_STUDENTS':
+            return action.students;
+        case 'ADD_STUDENT':
+            return [...students, action.student]
+        case 'DELETE_STUDENT':
+            return students.filter(student => +dtudent.generation !== +action.generation)
+        case 'UPDATE_STUDENT':
+            return students.map(student => {
+                if (+student.generation === +action.generation)
+                    return action.student;
+                else
+                    return student;
             })
     }
-    return bears;
+    return students;
 }
 
 const reducers = combineReducers({
-    bear:bearReducer,
-    form : formReducer,
+    student: studentReducer,
+    form: formReducer,
 })
 
 
-export const store = createStore(reducers,applyMiddleware(logger));
+export const store = createStore(reducers, applyMiddleware(logger));
