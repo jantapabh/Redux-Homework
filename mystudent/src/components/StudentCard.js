0f256b2b28e53665b080170871bb28ab350f8e47
jantapabh <jantapa2407@gmail.com>
2020-03-11 16:16:56 +0700

    add StudentCard

--- mystudent/src/components/StudentCard.js
+++ mystudent/src/components/StudentCard.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import './StudentCard.css';
 import axios from 'axios';
-import { usrDispatch, useSelector } from 'react-redux';
+import { usrDispatch, useSelector, useDispatch } from 'react-redux';
 
+const StudentCard = props => {
 
+    const form = useSelector(state => state.form)
+    const dispatch = useDispatch();
+
+    const deleteStudent = async () => {
+        await axios.delete(`https://localhost/api/students/${props. generation}`)
+        dispatch({ type: 'DELETE_STUDENT', generation: props. generation })
+    }
+
+    const updateStudent = async () => {
+
+    await axios.put(`https://localhost/api/students/${props. generation}`, form)
+    dispatch({ type: 'UPDATE_STUDENT',  generation: props. generation, Student: {...form,  generation: props. generation} })
+    }
+ 
+                             
+}
+
+export default StudentCard;
 
