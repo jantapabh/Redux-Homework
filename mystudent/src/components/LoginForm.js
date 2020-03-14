b37387b37c193be9082bd872b7c06d17aeec2b55
jantapabh <jantapa2407@gmail.com>
2020-03-15 01:12:53 +0700

    add form

--- mystudent/src/components/LoginForm.js
+++ mystudent/src/components/LoginForm.js
@@ -1,14 +1,6 @@
 import React, { useState, useEffect, Component } from 'react'
 import ReactDOM from 'react-dom';
-require('tls').DEFAULT_MIN_VERSION = 'TLSv1'   // since TLSv1.3 default disable v1.0 
-const express = require('express');
-const soap = require('soap');
-const bodyParser = require('body-parser')
 const url = 'https://passport.psu.ac.th/authentication/authentication.asmx?wsdl';
-const app = express()
-const router = express.Router()
-app.use(bodyParser.urlencoded({extended: false}), router)
-app.use(bodyParser.json, router)
 
 
 const LoginForm = () => {
@@ -18,9 +10,6 @@ const LoginForm = () => {
   <div>
 
       <h1>Login Form</h1>
-
-
-      
 
       <div>
 
