a41062689fdf0311791572a4bf52d69c79834d1d
jantapabh <jantapa2407@gmail.com>
2020-03-15 01:29:02 +0700

    add commit

--- mystudent/src/components/LoginForm.js
+++ mystudent/src/components/LoginForm.js
@@ -1,26 +1,25 @@
 import React, { useState, useEffect, Component } from 'react'
 import ReactDOM from 'react-dom';
 const url = 'https://passport.psu.ac.th/authentication/authentication.asmx?wsdl';
-
+import {BrowserRouter , Router, Switch, Link} from 'react-router-dom';
 
 const LoginForm = () => {
- 
-    return(
 
-  <div>
+    return (
 
-      <h1>Login Form</h1>
+        <div>
+            <h1>Login Form</h1>
+            <div>
+                <form action="post" method="post">
+                    Usename : <input type="text" name="username" placeholder="Put Your Username" /><br />
+                    Password: <input type="password" name="password" placeholder="Put YourPassword" /><br />
+                    <Link>
+                    <input type="submit" value="Submit" />
+                    </Link>
+                </form>
+            </div>
 
-      <div>
-          <form action="post" method="post">
-
-Usename : <input type="text" name="username" placeholder="Put Your Username" />
-Password: <input type="password" name="password" placeholder="Put YourPassword" />
-<input type="submit" value="Submit" />
-</form>
-      </div>
-      
-  </div>
+        </div>
 
     );
 
