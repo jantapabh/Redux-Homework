2935c52866408b4266d181190769460141037845
jantapabh <jantapa2407@gmail.com>
2020-03-15 03:24:16 +0700

    add css

--- mystudent/src/App.js
+++ mystudent/src/App.js
@@ -8,23 +8,10 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import LoginForm from './components/LoginForm'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
-import FacebookLoginWithButton from 'react-facebook-login';
+
 
 
 // App เป็นส่วนหลักในการไปยังหน้าต่าง ๆ 
-
-
-// const LoginForm = () => <h1>Home</h1>
-// const StudentList = () => <h1>About</h1>
-// const InputForm = () => <h1>Post</h1>
-
-const responseFacebook = (response) => {
-  console.log(response);
-}
-
-const componentClicked = () => {
-  console.log("Clicked!")
-}
 
 
 const App = () => {
@@ -40,15 +27,6 @@ const App = () => {
             <Route exact path="/studentlist" component={InputForm} />
           </Switch>
         </BrowserRouter>
-
-        <FacebookLoginWithButton
-          appId="526159711615139"
-          autoLoad
-          fields="name,email,picture"
-          onClick={componentClicked}
-          callback={responseFacebook}
-          icon="fa-facebook" />
-
       </Provider>
     </div>
 
--- mystudent/src/components/LoginForm.js
+++ mystudent/src/components/LoginForm.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect, Component } from 'react'
 import ReactDOM from 'react-dom';
-// import {BrowserRouter , Router, Switch, Link} from 'react-router-dom';
+import { BrowserRouter , Router, Switch, Link } from 'react-router-dom';
+import FacebookLoginWithButton from 'react-facebook-login';
+
 const url = 'https://passport.psu.ac.th/authentication/authentication.asmx?wsdl';
 
+
+
+const responseFacebook = (response) => {
+    console.log(response);
+  }
+  
+  const componentClicked = () => {
+    console.log("Clicked!")
+  }
+  
+  const UserScreen = ({ user }) => (
+    <>
+      <h1>Welcome {user.name}!</h1>
+      <p>{user.email}</p>
+      <img src={user.picture.data.url} height={user.picture.height} width={user.picture.width} alt="avatar" />
+    </>
+  )
+  
 
 const LoginForm = () => {
 
     return (
 
-        <div>
+        <div className="Main">
             <h1>Login Form</h1>
             <div>
                 <form action="post" method="post">
@@ -17,6 +37,15 @@ const LoginForm = () => {
                     <input type="submit" value="Submit" />
                 </form>
             </div>
+
+            
+        <FacebookLoginWithButton
+          appId="526159711615139"
+          autoLoad
+          fields="name,email,picture"
+          onClick={componentClicked}
+          callback={responseFacebook}
+          icon="fa-facebook" />
 
         </div>
 
