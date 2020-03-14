5f538d55f7d74a5c410bc2851158cfe41fcac6df
jantapabh <jantapa2407@gmail.com>
2020-03-14 19:40:33 +0700

    add InputForm

---
+++ mystudent/src/components/LoginForm.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+
+const LoginForm = () => {
+ 
+    return(
+
+  <div></div>
+
+    );
+
+}
+
+export default LoginForm;
