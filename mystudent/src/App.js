58447eaf381a5ec72dc8651f56439030840e3465
jantapabh <jantapa2407@gmail.com>
2020-03-15 03:00:14 +0700

    add facebooklogin

--- mystudent/src/App.js
+++ mystudent/src/App.js
@@ -8,6 +8,7 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import LoginForm from './components/LoginForm'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
+import FacebookLoginWithButton from 'react-facebook-login';
 
 
 // App เป็นส่วนหลักในการไปยังหน้าต่าง ๆ 
@@ -17,25 +18,38 @@ import { Provider } from 'react-redux'
 // const StudentList = () => <h1>About</h1>
 // const InputForm = () => <h1>Post</h1>
 
+const responseFacebook = (response) => {
+  console.log(response);
+}
+
+const componentClicked = () => {
+  console.log("Clicked!")
+}
 
 
 const App = () => {
 
   return (
-    
+
     <div className="Main">
       <Provider store={store}>
-      <BrowserRouter> 
-      <Switch>
-        <Route exact path="/" component={LoginForm} />
-        <Route exact path="/studentlist" component={StudentList} />
-        <Route exact path="/studentlist" component={InputForm} />
-      </Switch>
-      </BrowserRouter>
-      </Provider>
-      {/* <StudentList /> */}
-      {/* <InputForm /> */}
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={LoginForm} />
+            <Route exact path="/studentlist" component={StudentList} />
+            <Route exact path="/studentlist" component={InputForm} />
+          </Switch>
+        </BrowserRouter>
 
+        <FacebookLoginWithButton
+          appId="1206715649505081"
+          autoLoad
+          fields="name,email,picture"
+          onClick={componentClicked}
+          callback={responseFacebook}
+          icon="fa-facebook" />
+          
+      </Provider>
     </div>
 
   )
