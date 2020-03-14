d8793daa279d2fece5e3aae6efe0ad9497d6fb95
jantapabh <jantapa2407@gmail.com>
2020-03-14 23:39:00 +0700

    add commit

--- mystudent/src/components/LoginForm.js
+++ mystudent/src/components/LoginForm.js
@@ -1,13 +1,35 @@
 import React, { useState, useEffect, Component } from 'react'
 import ReactDOM from 'react-dom';
-
+equire('tls').DEFAULT_MIN_VERSION = 'TLSv1'   // since TLSv1.3 default disable v1.0 
+const express = require('express');
+const soap = require('soap');
+const bodyParser = require('body-parser')
+const url = 'https://passport.psu.ac.th/authentication/authentication.asmx?wsdl';
+const app = express()
+const router = express.Router()
+app.use(bodyParser.urlencoded({extended: false}), router)
+app.use(bodyParser.json, router)
 
 
 const LoginForm = () => {
  
     return(
 
-  <div>LoginForm</div>
+  <div>
+
+      <h1>Login Form</h1>
+
+
+      
+
+      <div>
+
+Hiii
+
+
+      </div>
+      
+  </div>
 
     );
 
--- mystudent/src/components/StudentList.js
+++ mystudent/src/components/StudentList.js
@@ -2,59 +2,62 @@ import React, { useState, useEffect } from 'react'
 import StudentCard from './StudentCard';
 import './StudentList.css';
 import axios from 'axios'
-import {useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import InputForm from './InputForm'
 
 // ส่วนแสดงรายชื่อนักเรียนทั้งหมดที่มาจาก server
 
 const StudentList = props => {
 
-    const students = useSelector(state => state.student);
-    console.log('Student = = = ' +students);
-    const dispatch = useDispatch();
+  const students = useSelector(state => state.student);
+  console.log('Student = = = ' + students);
+  const dispatch = useDispatch();
 
-    const getStudents = async () => {
+  const getStudents = async () => {
 
-        const result = await axios.get(`http://localhost:8000/api/students`)
-        const action = { type: 'GET_STUDENTS' , students: result.data };
-        dispatch(action)
-    }
+    const result = await axios.get(`http://localhost:8000/api/students`)
+    const action = { type: 'GET_STUDENTS', students: result.data };
+    dispatch(action)
+  }
 
-    useEffect(() => {
+  useEffect(() => {
 
-        getStudents()
+    getStudents()
 
   }, [])
 
-  if(!students || !students.length)
+  if (!students || !students.length)
 
-  return (
+    return (
 
-    <h1>Not Student Now</h1>
-  )
+      <h1>Not Student Now</h1>
+    )
 
   return (
 
     <div>{
-
-        students.map((student, index) => (
-
-            <div key={index} style={{margin: 5}}>
-                <StudentCard {...student} updateStudent={() => props.updateStudent(student.generation)} deleteStudent = {() => props.deleteStudent(student.generation)} />
-        
-            </div>
-            <div>
-             <InputForm />
-             </div>
-                
-        ))
-        }
-
-
+      students.map((student, index) => (
+        <div key={index} style={{ margin: 5 }}>
+          <StudentCard {...student} updateStudent={() => props.updateStudent(student.generation)} deleteStudent={() => props.deleteStudent(student.generation)} />
         </div>
+        // <div>
+        //   <InputForm />
+        // </div>
+
+      ))
+
+
+    }
+
+      <div>
+        <InputForm />
+      </div>
+
+
+    </div>
 
   )
-    
+
 }
 
 export default StudentList;
