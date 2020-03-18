344341edf384783c22d3c6f5dfcfd320612ffabd
jantapabh <jantapa2407@gmail.com>
2020-03-18 22:16:59 +0700

    add login

--- mystudent/src/components/LoginForm.js
+++ mystudent/src/components/LoginForm.js
@@ -1,36 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Router } from 'express';
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import StudentList from './StudentList'
 import InputForm from './InputForm'
 
-const LoginForm = props => {
-
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [loginStatus, setLoginStatus] = useState('Not yet logged in');
-    
-    const login = async () => {
-        const res = await axios.post('/api/psu', { username, password });
-        setLoginStatus(JSON.stringify(res.data))
-        console.log(res)
-    }
+const LoginForm = () => {
 
     return (
 
         <div>
-            <Route exact path="/studentlist" component={StudentList} />
-            <Route exact path="/studentlist" component={InputForm} />
- </div>
+            Login
+        </div>
 
     )
 }
 
 export default LoginForm
 
-{/* <form action="/" method="post">
- Username: <input type="text" name="username" /> <br>
- Password: <input type="password" name="password" /> <br>
- <input type="submit" value="Submit">
-</form> */}
--- soap.js
+++ soap.js
@@ -1,54 +1,48 @@
-const express = require("express")
-const soap = require("soap")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const url = "https://passport.psu.ac.th/authentication/authentication.asmx?wsdl"
-
+require('tls').DEFAULT_MIN_VERSION = 'TLSv1'   // since TLSv1.3 default disable v1.0 
+const express = require('express');
+const soap = require('soap');
+const bodyParser = require('body-parser')
+const url = 'https://passport.psu.ac.th/authentication/authentication.asmx?wsdl';
 const app = express()
+const router = express.Router()
+app.use(bodyParser.urlencoded({extended: false}), router)
+app.use(bodyParser.json, router)
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(express.json())
+const out = `
+<html>
+<body>
+  <h2>PSU Passport Authentication (SOAP) </h2>
+ <form action="/" method="post">
+ Username: <input type="text" name="username" /> <br>
+ Password: <input type="password" name="password" /> <br>
+ <input type="submit" value="Submit">
+</form>
+</body>
+</html> 
+`
 
-app.get("/", (req, res) => {
-      res.send("test")
-})
-app.post("/", (req, res) => {
-      soap.createClient(url, (err, client) => {
-            if (err) {
-                  console.error(err)
-                  res.sendStatus(401)
-            } else {
-                  let user = {}
-                  user.username = req.body.username
-                  user.password = req.body.password
-                  client.GetStaffDetails(user, function(err, response) {
-                        // client.GetStudentDetails(args, function(err, response) {
-                        if (err) {
-                              console.error(err)
-                              res.sendStatus(401)
-                        } else {
-                              console.log(response)
-                              try {
-                                    if (
-                                          response.GetStaffDetailsResult
-                                                .string[0]
-                                    ) {
-                                          res.send(
-                                                response.GetStaffDetailsResult
-                                                      .string[0]
-                                          )
-                                    } else {
-                                          res.sendStatus(401)
-                                    }
-                              } catch (error) {
-                                    res.sendStatus(401)
-                              }
-                        }
-                  })
-            }
-      })
-})
+router.route('/')
+   .get((req, res) => {
+       res.send(out)
+   })
+   .post((req, res) => {
+       soap.createClient(url, (err, client) => {
+           if (err) console.error(err);
+           else {
+               let user = {}
+               user.username = req.body.username
+               user.password = req.body.password
 
-app.listen(80, () => console.log("Server is ready!"))
+               client.GetStaffDetails(user, function (err, response) {
+                   // client.GetStudentDetails(args, function(err, response) {
+                   if (err) console.error(err);
+                   else {
+                       console.log(response);
+                       res.send(response);
+                   }
+               });
+           }
+       });
+   })
+
+app.listen(80, () => console.log('Server is ready!'))
