25470449376789c07875c7265e382f0911f85224
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-30 16:58:40 +0700

    add commit

--- mystudent/src/components/LoginForm.js
+++ mystudent/src/components/LoginForm.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Route, Switch, BrowserRouter } from 'react-router-dom'
+
 import StudentList from './StudentList'
 import InputForm from './InputForm'
 import FacebookLogin from 'react-facebook-login';
 import './LoginForm.css';
-// import { bindActionCreators } from 'react-redux/node_modules/redux';
+
+
 import { AuthActions } from '../redux/store';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux'
-import config from '../config'
+import { Button, Form, Card } from 'react-bootstrap';
 
 
-const LoginForm = () => {
+const LoginForm = (props) => {
 
-    const actions = bindActionCreators(AuthActions, useDispatch());
+   
+    const actions = bindActionCreators({ ...AuthActions }, useDispatch())
     const [facebookLink, setFacebookLink] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     const getFacebookLink = async () => {
 
-        const res = await axios.get(`${config.apiUrl}/auth/facebook`);
+        const res = await axios.get(`http://localhost/api/auth/facebook`);
         setFacebookLink(res.data);
     }
 
@@ -27,29 +31,69 @@ const LoginForm = () => {
         getFacebookLink()
     }, []);
 
-    const loginPSU = e => {
+    const loginPSU = (e) => {
 
-        e.prevenDefault();
-        const { username, password } = e.target.elements
+        e.preventDefault();
         actions.loginPSU(username.value, password.value)
     }
     return (
-
-        <div>
+        <div   className="M">
             <div>
-                Facebook Login Click <a href={facebookLink}>Here</a>
-            </div>
-            <h1>PSU PASSPORT</h1>
-            <div>
-                <form onSubmit={loginPSU}>
-                    Username: <input type="text" name="username" placeholder="Put Usename" /> <br />
-                    PassWord: <input type="password" name="password" placeholder="Put Password" /> <br />
-                    <input type="submit" value="Submit" />
-                </form>
+            <h1 className="h1">LOGIN FORM</h1>
+                <div>
+                    <br />
+                    <br />
+                    <div className="last">
+                        <Card style={{ display: 'flex', justifyContent: 'center' }}>
+                            <Card.Body>
+                                <Card.Title className="topic">Facebook LOGIN</Card.Title>
+                                <br />
+                                <br />
+                        
+                                <Button href={facebookLink}>LOGIN</Button>
+                                <br />
+                                <br />
+                            </Card.Body>
+                        </Card>
+                        </div>
+                    {' '}
+                    <div>
+                        <div>
+                            <form>
+                                <div>
+                                    <div>
+                                        <div className="last">
+                                            <Card>
+                                                <Card.Body>
+                                                    <Card.Title className="topic">PSU LOGIN</Card.Title>
+                                                    <Card.Text>
+                                                        <Form>
+                                                            <Form.Group controlId="formBasicEmail">
+                                                                <Form.Label>Username</Form.Label>
+                                                                <br />
+                                                                <Form.Control type="text" placeholder="Put Username" name="username" onChange={(e)=>setUsername(e.target.value)}/>
+                                                            </Form.Group>
+                                                            <Form.Group controlId="formBasicPassword">
+                                                                <Form.Label>Password</Form.Label>
+                                                                <br />
+                                                                <Form.Control type="password" placeholder="Put Password" name="password" onChange={(e)=>setPassword(e.target.value)}/>
+                                                            </Form.Group>
+                                                        </Form>
+                                                    </Card.Text>
+                                                    <Button variant="outline-primary" onClick={loginPSU}>LOGIN</Button>
+                                                </Card.Body>
+                                            </Card>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                   
+                </div>
             </div>
         </div>
     )
+
 }
-
 export default LoginForm;
-
